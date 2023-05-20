e8f4fdd4c4bd7d5322acf73b1d55adb4b62bfaaf
Jubayer9 <121636287+Jubayer9@users.noreply.github.com>
2023-05-20 15:05:29 +0600

    Delete operation added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -51,6 +51,13 @@ const ToyCollection = client.db('toyDB').collection('toy')
       console.log(newToy);
       const result = await ToyCollection.insertOne(newToy);
       res.send(result)
+    })
+
+    app.delete('/addToy/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await ToyCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
